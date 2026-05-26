8bc9744d752776b71d621cf5a143972b961f00dd
Samueeeelll <samuel_silvany@estudante.sesisenai.org.br>
2026-05-26 13:38:23 -0300

    26-05

---
+++ Back-End/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const app = express();
+
+
+
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'Replace',
+  password: '123456',
+  port: 5432,
+});
+
+
+module.exports - pool;
